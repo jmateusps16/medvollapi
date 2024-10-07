e17d74d81c3d939506c5bbda435045016fd1c264
Mateus Palmeira <jmateusps16@gmail.com>
2024-10-07 19:16:59 -0300

    domain e repositorio

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,24 @@
 package med.voll.api.controller;
 
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    private final MedicoRepository repository;
+
+    @Autowired
+    public MedicoController(MedicoRepository repository) {
+        this.repository = repository;
+    }
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
