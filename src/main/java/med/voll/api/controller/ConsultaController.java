f2b815d0e9305e83ad83d98f1b6058c9154e71a1
Mateus Palmeira <jmateusps16@gmail.com>
2024-11-07 00:51:44 -0300

    agenda e cancelamento consulta sem validar regra de negocio

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,22 +2,37 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    private final AgendaDeConsultas agendaDeConsultas;
+
+    @Autowired
+    public ConsultaController(AgendaDeConsultas agendaDeConsultas) {
+        this.agendaDeConsultas = agendaDeConsultas;
+    }
+
     @PostMapping
     @Transactional
     public ResponseEntity<DadosDetalhamentoConsulta> agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
         System.out.println(dados);
+        agendaDeConsultas.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity<Void> cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+        agendaDeConsultas.cancelar(dados);
+        return ResponseEntity.noContent().build();
     }
 }
