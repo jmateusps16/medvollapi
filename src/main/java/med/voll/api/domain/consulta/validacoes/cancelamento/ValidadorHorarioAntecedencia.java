d1e3cf6f2b630ad6660059b4d6b45cf431f8e3fc
Mateus Palmeira <jmateusps16@gmail.com>
2024-11-07 20:32:31 -0300

    validações de cancelamento

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,33 @@
+package med.voll.api.domain.consulta.validacoes.cancelamento;
+
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+import med.voll.api.domain.exceptions.ValidacaoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component("ValidadorHorarioAntecedenciaCancelamento")
+public class ValidadorHorarioAntecedencia implements ValidadorCancelamentoDeConsulta {
+
+    private final ConsultaRepository repository;
+
+    @Autowired
+    public ValidadorHorarioAntecedencia(ConsultaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void validar(DadosCancelamentoConsulta dados) {
+        var consulta = repository.getReferenceById(dados.idConsulta());
+        var agora = LocalDateTime.now();
+        var diferencaEmHoras = Duration.between(agora, consulta.getData()).toHours();
+
+        if (diferencaEmHoras < 24) {
+            throw new ValidacaoException("Consulta somente pode ser cancelada com antecedência mínima de 24 horas.");
+        }
+    }
+}
